38a59f29f3a219cff45cb0a1bea0b8041577957c
bestcaptchasolver <bcsolver@gmail.com>
2018-11-02 08:25:13 +0200

    error handling and others

--- lib/bestcaptchasolver.rb
+++ lib/bestcaptchasolver.rb
@@ -15,21 +15,19 @@ USER_AGENT = 'rubyAPI1.0'
 class Bestcaptchasolver
   def initialize(access_token)
     @_access_token = access_token
-    @_affiliate_id = nil
     @_headers = {"User-Agent" => USER_AGENT}
-  end
-
-  # set affiliate id
-  def set_affiliate_id(id)
-    @_affiliate_id = id
   end
 
   # get accounts balance
   def account_balance
     url = '%s/user/balance?access_token=%s' % [BASE_URL, @_access_token]
     # make request
-    response_text = JSON.parse(Net::HTTP.get URI(url))
-    response_text['balance']
+    r = JSON.parse(Net::HTTP.get URI(url))
+    # check for error
+    if r.key? 'error'
+      raise(r['error'])
+    end
+    r['balance']
   end
 
   # submit b64 encoded image captcha
@@ -37,16 +35,17 @@ class Bestcaptchasolver
     url = '%s/captcha/image' % [BASE_URL]
     uri = URI.parse(url)
     d["access_token"] = @_access_token
-    # check for affiliate ID
-    if @_affiliate_id != nil
-      d['affiliate_id'] = @_affiliate_id
-    end
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     req = Net::HTTP::Post.new(uri.request_uri, @_headers)
     res = http.request(req, URI.encode_www_form(d))
 
-    JSON.parse(res.body)['id']    # get response body
+    r = JSON.parse(res.body)
+    # check for error
+    if r.key? 'error'
+      raise(r['error'])
+    end
+    r['id']    # get ID
   end
 
   # submit recaptcha
@@ -54,10 +53,6 @@ class Bestcaptchasolver
     url = '%s/captcha/recaptcha' % [BASE_URL]
     uri = URI.parse(url)
     d["access_token"] = @_access_token
-    # check for affiliate ID
-    if @_affiliate_id != nil
-      d['affiliate_id'] = @_affiliate_id
-    end
     # check for proxy
     if d.key? 'proxy'
       d['proxy_type'] = 'HTTP'
@@ -68,18 +63,27 @@ class Bestcaptchasolver
     req = Net::HTTP::Post.new(uri.request_uri, @_headers)
     res = http.request(req, URI.encode_www_form(d))
 
-    JSON.parse(res.body)['id']    # get response body
+    r = JSON.parse(res.body)
+    # check for error
+    if r.key? 'error'
+      raise(r['error'])
+    end
+    r['id']    # get ID
   end
 
   # retrieve captcha by ID
   def retrieve(id)
     url = '%s/captcha/%s?access_token=%s' % [BASE_URL, id, @_access_token]
     # make request
-    response_text = JSON.parse(Net::HTTP.get URI(url))
-    if response_text['status'] === 'pending'
+    r = JSON.parse(Net::HTTP.get URI(url))
+    # check for error
+    if r.key? 'error'
+      raise(r['error'])
+    end
+    if r['status'] === 'pending'
       return nil
     end
-    response_text
+    r   # return response
   end
 
   # set captcha bad using ID
@@ -93,7 +97,12 @@ class Bestcaptchasolver
     req = Net::HTTP::Post.new(uri.request_uri, @_headers)
     res = http.request(req, URI.encode_www_form(d))
 
-    JSON.parse(res.body)['status'] # return body
+    r = JSON.parse(res.body)
+    # check for error
+    if r.key? 'error'
+      raise(r['error'])
+    end
+    r['status']   # return status
   end
 end
 
